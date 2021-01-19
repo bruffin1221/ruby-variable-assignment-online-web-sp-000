a0565d4c16ac10a25c40d214c7b510efb926ca74
Bruce Ruffin <bruffin1221@gmail.com>
2021-01-19 05:01:31 +0000

    Automatically backed up by Learn

---
+++ 02_add_buyers.sql
@@ -0,0 +1,5 @@
+INSERT INTO buyers(name, city, product, cost) VALUES ("Rob Deniro", "Johnson City, KS", "tractor", 45000.01);
+INSERT INTO buyers(name, city, product, cost) VALUES ("Michael Jordan","Yakima, WA", "combine", 100000.53);
+INSERT INTO buyers(name, city, product, cost) VALUES ("Kobe Bryant", "Madras, OR", "sheers", 2000.10);
+INSERT INTO buyers(name, city, product, cost) VALUES ("Von Miller", "Yuma, AZ", "Balers", 3012.75);
+INSERT INTO buyers(name, city, product, cost) VALUES ("Nokola Jokic", "Cheyanne, WY", "Wheel Rakes", 10000.12);
